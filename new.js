2d86331a5eb77a490c97883a40fbb6efa6bedee3
J-Bannach <JohannaBannach@gmail.com>
2021-04-27 20:07:14 +0200

    add radio button function to check

--- new.js
+++ new.js
@@ -26,8 +26,37 @@ const formElement = document.querySelector(".form");
 
 formElement.onsubmit = function (event) {
   event.preventDefault();
+  const taskNameInput = document.querySelector(".form__input");
   const checkedDateInput = document.querySelector(
     ".radio-group__input:checked"
   );
-  console.log(taskNameInput.value, checkedDateInput.value);
+
+  // if (checkedDateInput && taskNameInput.value !== "") {
+  //   console.log(taskNameInput.value + " " + checkedDateInput.value);
+  // } else {
+  //   alert("Please enter task and date");
+  //   return;
+  // }
+
+  if (!taskNameInput.value) {
+    alert("Please enter task");
+  }
+  if (!checkedDateInput) {
+    alert("Please choose date");
+  }
+  // console.log(taskNameInput.value, checkedDateInput.value);
 };
+
+/* //// IF STATEMENT //// */
+
+// if (taskNameInput.value !== null && checkedDateInput.value !== "") {
+//   console.log(taskNameInput.value + " " + checkedDateInput.value);
+// } else {
+//   alert("Please enter task and date");
+// }
+
+// if (!checkedDateInput.value) {
+//   alert("Please enter task & date");
+// } else {
+//   console.log(taskNameInput.value, checkedDateInput.value);
+// }
