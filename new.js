3273e6cf15d0420ad3fc5dbb1aec4e402d33a9bb
Johanna <78214585+J-Bannach@users.noreply.github.com>
2021-04-29 14:30:46 +0200

    Add new task (JS) (#11)
    * add new.js file
    
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript
    
    * log new task on form submit
    
    * add radio button function to check
    
    * add JS array function
    
    * Add link to dashboard.html and create empty array if tasklist null

---
+++ new.js
@@ -0,0 +1,72 @@
+// const onSubmit = document.querySelector(".form__submit");
+
+// onSubmit.addEventListener("click", showTask);
+
+// function showTask() {
+//   alert("Show task");
+// }
+
+// const taskNameInput = document.querySelector(".form__input");
+// const formSubmitInput = document.querySelector(".form__submit");
+
+// function handleClick() {
+//   console.log(taskNameInput.value);
+// }
+
+// formSubmitInput.onclick = function () {
+//   console.log(taskNameInput.value);
+// };
+
+// const taskNameInput = document.querySelector(".radio-group__input:checked").value;
+
+/* ///// FORMS //// */
+
+const taskNameInput = document.querySelector(".form__input");
+const formElement = document.querySelector(".form");
+
+// WEITERLEITUNG
+
+function goToPage(href) {
+  location.href = href;
+}
+// WEITERLEITUNG
+
+formElement.onsubmit = function (event) {
+  event.preventDefault();
+  const taskNameInput = document.querySelector(".form__input");
+  const checkedDateInput = document.querySelector(
+    ".radio-group__input:checked"
+  );
+
+  if (!taskNameInput.value && !checkedDateInput) {
+    alert("Please enter task & date");
+  } else if (!taskNameInput.value) {
+    alert("Please enter task.");
+  } else if (!checkedDateInput) {
+    alert("Please choose date");
+  } else {
+    const tasks = {
+      description: taskNameInput.value,
+      date: checkedDateInput.value,
+    };
+    // //////////////////////////////////////////////////////////////////
+    // Get and parse taskList; create empty array if taskList is null
+    // //////////////////////////////////////////////////////////////////
+
+    const taskListJSON = localStorage.getItem("taskList");
+
+    let taskList = JSON.parse(taskListJSON);
+    if (taskList === null) {
+      taskList = [];
+    }
+    console.log(taskList);
+
+    taskList.push(tasks);
+    console.log(taskList);
+
+    const newTaskListJSON = JSON.stringify(taskList);
+    localStorage.setItem("taskList", newTaskListJSON);
+  }
+
+  goToPage("./dashboard.html");
+};
